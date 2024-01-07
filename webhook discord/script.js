a19bc82fe507fca5b7a5938c0380a89475fbaa76
ariyanPutra <149260521+ariyanPutra@users.noreply.github.com>
2024-01-07 07:54:53 +0700

    Add files via upload

---
+++ webhook discord/script.js
@@ -0,0 +1,26 @@
+function sendMessage() {
+    const webhookURL = document.getElementById('idWebhook').value;
+    const message = document.getElementById('message').value;
+
+    if (webhookURL && message) {
+        fetch(webhookURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ content: message })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            alert('Message sent successfully!');
+            // Tambahkan logika lainnya di sini jika diperlukan
+        })
+        .catch(error => {
+            alert('There was a problem sending the message:', error);
+        });
+    } else {
+        alert('Webhook URL and message are required');
+    }
+}
